000d657fe21495d249c4c9a05986a4203f27a438
Itseasymike <michaelrivera00@icloud.com>
2016-03-19 23:47:04 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown_with_sleep(time)
+	while time > 0
+		puts "Go to Sleep"
+		time -= 1
+		sleep(1)
+	end
+	"HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown(seconds)
+	seconds = 10
+	while seconds > 0 
+		puts "#{seconds} SECOND(S)!"
+		seconds -= 1
+	end
+	"HAPPY NEW YEAR!"
 end
